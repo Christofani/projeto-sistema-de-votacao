6e2c16900aae4be160fb69ca4dda161d023351a5
Rodrigo Christofani <christofanir@gmail.com>
2024-07-31 21:29:49 -0300

    Iniciando Projeto.

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,36 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+/**
+ * The type Pessoa.
+ */
+public abstract class Pessoa {
+
+  /**
+   * The Nome.
+   */
+  protected  String nome;
+
+  /**
+   * Instantiates a new Pessoa.
+   */
+  public  Pessoa() {
+  }
+
+  /**
+   * Gets nome.
+   *
+   * @return the nome
+   */
+  public String getNome() {
+    return  nome;
+  }
+
+  /**
+   * Sets nome.
+   *
+   * @param nome the nome
+   */
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
