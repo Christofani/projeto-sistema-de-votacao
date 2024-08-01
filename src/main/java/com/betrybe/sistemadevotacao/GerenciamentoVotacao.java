32af1ce0e89b18e9940817d7972eb61d221ed456
Rodrigo Christofani <christofanir@gmail.com>
2024-07-31 21:56:18 -0300

    Resolução requisito 4.

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,8 +1,43 @@
 package com.betrybe.sistemadevotacao;
 
+
+import java.util.ArrayList;
+
 /**
  * The type Gerenciamento votacao.
  */
-public class GerenciamentoVotacao {
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfsComputados;
+
+  /**
+   * Instantiates a new Gerenciamento votacao.
+   */
+  public GerenciamentoVotacao() {
+    this.pessoasCandidatas = new ArrayList<>();
+    this.pessoasEleitoras = new ArrayList<>();
+    this.cpfsComputados = new ArrayList<>();
+  }
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    // Implementação futura
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    // Implementação futura
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    // Implementação futura
+  }
+
+  @Override
+  public void mostrarResultado() {
+    // Implementação futura
+  }
 }
